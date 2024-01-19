f8f8d40cd492b0b7ef5d7867757353cf183c4a63
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2024-01-18 22:56:49 -0600

    Classes

--- 001-fundamentals/Program.cs
+++ 001-fundamentals/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**
+// Three way to create an object
+Sale sale1 = new Sale();
+var sale2 = new Sale();
+Sale sale3 = new();
+*/
+
+var sale =  new Sale(15);
+var message = sale.GetInfo();
+
+Console.WriteLine(message);
+
+class Sale
+{
+    public decimal Total { get; set; }
+    private decimal _some;
+
+    public Sale(decimal total){
+        Total = total;
+    }
+
+    public string GetInfo() {
+        return "Total is " + Total;
+    }
+}
